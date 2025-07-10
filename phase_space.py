a585bd9077b6e3b1e388dd084a7c0345e10689f8
Ebony Rose <184883989+Eb0nyR0se@users.noreply.github.com>
2025-07-10 12:11:17 -0400

    Update phase_space.py
    Signed-off-by: Ebony Rose <184883989+Eb0nyR0se@users.noreply.github.com>

--- phase_space.py
+++ phase_space.py
@@ -1,7 +1,5 @@
 # File: phase_space.py
 
-# File: phase_space.py
-
 import matplotlib.pyplot as plt
 from main import signal_spiral_encrypt
 
@@ -49,5 +47,5 @@ if __name__ == "__main__":
     key = prompt_float("Enter key (float)", 0x4242424242424242)
     rounds = prompt_int("Enter number of rounds", 200)
 
-    phase_space_plot(0x1122334455, 0x4242424242424242, rounds=200)
+    phase_space_plot(0x1122334455, 0x4242424242424242, rounds=100)
 
